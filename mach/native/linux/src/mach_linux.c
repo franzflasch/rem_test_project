7f94587bef21fc8f1a5e6aac9c0a0f383f26677f
Franz Flasch <franz.flasch@gmx.at>
2016-09-01 19:45:23 +0200

    added native machine

---
+++ mach/native/linux/src/mach_linux.c
@@ -0,0 +1,49 @@
+/*
+ * STM32F1_blinky.c
+ *
+ *  Created on: Feb 26, 2015
+ *      Author: fflasch
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <OMM_machine_common.h>
+#include <gpio_common.h>
+#include <unistd.h>
+#include <drv_core.h>
+
+void OMM_busy_delay(uint64_t val)
+{
+	usleep(val*1000);
+}
+
+
+static void linux_gpio_set(gpio_controller_t *gpio, uint8_t pin, uint8_t val)
+{
+	printf("Setting gpio %d to %d\n", pin, val);
+}
+
+
+OMM_machine_t __attribute__((weak)) *machine_setup(void)
+{
+	static OMM_machine_t machine =
+	{
+			.name = "OpenMicroMake"
+	};
+
+	static gpio_controller_t gpio;
+	static OMM_platform_devices pdevs[] =
+	{
+			{"gpio_0", &gpio },
+			{NULL, NULL}
+	};
+
+	print_registered_drivers();
+
+	GPIO_init_controller(&gpio, ACTIVE_HIGH, 1, linux_gpio_set, NULL, NULL);
+
+	machine.pdev_list = &pdevs[0];
+
+	return &machine;
+}
+
