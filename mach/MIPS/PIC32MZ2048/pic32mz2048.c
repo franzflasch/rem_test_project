624ec02a75b1dcf4a84082e93d49ce6c68d52fd1
Franz Flasch <franz.flasch@gmx.at>
2017-01-27 20:51:28 +0100

    Add support for PIC32MZ

--- mach/MIPS/PIC32MZ2048/pic32mz2048.c
+++ mach/MIPS/PIC32MZ2048/pic32mz2048.c
@@ -6,7 +6,7 @@
  */
 
 #include <stdint.h>
-#include <mx2/sfr.h>
+#include <mz2048/sfr.h>
 #include <config.h>
 
 #include <stdlib.h>
@@ -14,12 +14,16 @@
 #include <gpio_common.h>
 
 // Configuration bits:
-// - Internal 8Mhz OSC with PLL -> 40Mhz
+// - 12MHz DIV1 MUL32 DIV2 = 192000000
 // - JTAG enabled
-DECLARE_CONFIG(0, 0x7FFFFFFF); 
-DECLARE_CONFIG(1, 0xFFF4EFF9); 
-DECLARE_CONFIG(2, 0xFFF9F9D9); 
-DECLARE_CONFIG(3, 0xFFFFFFFF);
+DECLARE_CONFIG(0, 0x40007777); 
+DECLARE_CONFIG(1, 0xdff4c7ff); 
+DECLARE_CONFIG(2, 0xc0011fa0); 
+DECLARE_CONFIG(3, 0x7b000000);
+DECLARE_CP(0, 0x10000000);
+
+#define LED_TRIS    TRISHbits.TRISH2    // macro for direction register bit of the LED pin
+#define LED_LAT     LATHbits.LATH2      // macro for output register bit of the LED pin
 
 void OMM_busy_delay(uint32_t val)
 {
@@ -28,13 +32,9 @@ void OMM_busy_delay(uint32_t val)
             __asm__("nop");
 }
 
-static void MACH_PIC32MX2_gpio_setup(void)
+static void MACH_PIC32MZ2048_gpio_setup(void)
 {
-   // set RB0 and RB1 as output
-   TRISBCLR = 3;
-
-   // set outputs to zero
-   LATBCLR = 3;
+    LED_TRIS = 0;
 }
 
 static void set_gpio(uint8_t pin, uint8_t val)
@@ -43,22 +43,16 @@ static void set_gpio(uint8_t pin, uint8_t val)
     {
         case 0:
             if(val == GPIO_COMMON_HIGH)
-                LATBINV |= (1<<0);
+                LED_LAT |= (1<<0);
             else if (val == GPIO_COMMON_LOW)
-                LATBINV &= ~(1<<0);
-            break;
-        case 1:
-            if(val == GPIO_COMMON_HIGH)
-                LATBINV |= (1<<1);
-            else if (val == GPIO_COMMON_LOW)
-                LATBINV &= ~(1<<1);
+                LED_LAT &= ~(1<<0);
             break;
         default:
             break;
     }
 }
 
-static void MACH_PIC32MX2_gpio_set(gpio_controller_t *gpio, uint8_t pin, uint8_t val)
+static void MACH_PIC32MZ2048_gpio_set(gpio_controller_t *gpio, uint8_t pin, uint8_t val)
 {
     if(gpio->mode == ACTIVE_LOW)
     {
@@ -73,9 +67,10 @@ static void MACH_PIC32MX2_gpio_set(gpio_controller_t *gpio, uint8_t pin, uint8_t
     }
 }
 
-
 OMM_machine_t __attribute__((weak)) *machine_setup(void)
 {
+    //SYSTEMConfigPerformance(192000000L);
+
     static OMM_machine_t machine =
     {
             .name = "OpenMicroMake"
@@ -88,10 +83,8 @@ OMM_machine_t __attribute__((weak)) *machine_setup(void)
             {NULL, NULL}
     };
 
-    //SYSTEMConfigPerformance(40000000L);
-
-    MACH_PIC32MX2_gpio_setup();
-    GPIO_init_controller(&gpio, ACTIVE_HIGH, 1, MACH_PIC32MX2_gpio_set, NULL, NULL);
+    MACH_PIC32MZ2048_gpio_setup();
+    GPIO_init_controller(&gpio, ACTIVE_HIGH, 1, MACH_PIC32MZ2048_gpio_set, NULL, NULL);
 
     machine.pdev_list = &pdevs[0];
 
--- test_project/src/blinky.c
+++ test_project/src/blinky.c
@@ -4,25 +4,24 @@
 #include <cordic.h>
 #include <stdio.h>
 
-
 int main (void)
 {
-	int sin = 0;
-	int cos = 0;
-	uint8_t gpio_val = 0;
-	gpio_controller_t *gpio = NULL;
+    int sin = 0;
+    int cos = 0;
+    uint8_t gpio_val = 0;
+    gpio_controller_t *gpio = NULL;
 
-	OMM_machine_t *machine = machine_setup();
+    OMM_machine_t *machine = machine_setup();
 
-	gpio = OMM_get_pdev_by_name(machine, "gpio_0");
-	//gpio = OMM_get_pdev_by_index(machine, 0);
-	cordic_sincos(3, 10, &sin, &cos);
+    gpio = OMM_get_pdev_by_name(machine, "gpio_0");
+    //gpio = OMM_get_pdev_by_index(machine, 0);
+    cordic_sincos(3, 10, &sin, &cos);
 
     while (1)
     {
-    	gpio_val ^= 1 << 0;
-    	GPIO_set(gpio, 0, gpio_val);
-    	GPIO_set(gpio, 1, !gpio_val);
-    	OMM_busy_delay(1500);
+        gpio_val ^= 1 << 0;
+        GPIO_set(gpio, 0, gpio_val);
+        GPIO_set(gpio, 1, !gpio_val);
+        OMM_busy_delay(1500);
     }
 }
